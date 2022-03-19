cf772ec0b7ed4e2d25665e76e395a1c5ec760eee
HNgan <hnagn2003@gmail.com>
2022-03-19 15:59:50 +0700

    sau

--- Specifications.h
+++ Specifications.h
@@ -7,6 +7,14 @@ const int COLS = 10;
 const int SCREEN_WIDTH = 1920;
 const int SCREEN_HEIGHT = 1030;
 
+const SDL_Color I_cBLOCK = {0, 209, 232};
+const SDL_Color L_cBLOCK = {0, 126, 229};
+const SDL_Color J_cBLOCK = {218, 147, 0};
+const SDL_Color O_cBLOCK = {232, 214, 0};
+const SDL_Color S_cBLOCK = {0, 222, 61};
+const SDL_Color T_cBLOCK = {187, 0, 222};
+const SDL_Color Z_cBLOCK = {232, 0, 0};
+
 struct Point{
     int x;
     int y;
@@ -16,4 +24,18 @@ struct Point{
     }
 };
 
+struct PointOnGrid{
+    int x;
+    int y;
+    PointOnGrid(int _x = 0, int _y = 0){
+        x = _x;
+        y = _y;
+    }
+    int xPosOnGrid(){
+        return (SCREEN_WIDTH - COLS*TILE_SIZE) / 2 + x*TILE_SIZE;
+    } 
+    int yPosOnGrid(){
+        return (SCREEN_HEIGHT - ROWS*TILE_SIZE) / 2 + y*TILE_SIZE;
+    }
+};
 #endif
--- Tetromino.h
+++ Tetromino.h
@@ -4,7 +4,7 @@
 #include <SDL.h>
 #include <stdio.h>
 #include <SDL_image.h>
-
+#include "Specifications.h"
 class block{
     public:
         render(int xPos, int yPos);
@@ -14,14 +14,36 @@ class block{
 };
 
 class Tetromino{
-    public:
-
     private:
         SDL_Color color;
         int size;
         bool matrix[4][4];
-        double x, y;
-        
+    public:
+        Tetromino(){};
+        // Tetromino(SDL_Color _color, int _size, bool _matrix[4][4])
+        // {
+        //     color = _color;
+        //     size = _size;
+        //     matrix[4][4] = _matrix[4][4];
+        // };
 };
+
+// enum Tetrominos{
+//     I_BLOCK,
+//     L_BLOCK,
+//     J_BLOCK,
+//     O_BLOCK,
+//     S_BLOCK,
+//     T_BLOCK,
+//     Z_BLOCK,
+//     total_of_Tetrominos
+// };
+// Tetromino Tetrads[7];
+
+const Tetromino I_BLOCK = {I_cBLOCK, 4, {{0,0,1,0} // L BLOCK
+,{1,1,1,0}
+,{0,0,0,0}
+,{0,0,0,0}}};
+
 
 #endif
